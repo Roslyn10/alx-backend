50f4f0c9166b869e6167bc3af89901d669061bba
Roslyn10 <roslynclewis1004@gmail.com>
2024-09-02 12:30:38 +0200

    Fixing the declaration of the index_page function, making it part of the server class

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -34,14 +34,17 @@ class Server:
         return self.__dataset
 
 
-def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-    """
-    retrieves paginated data
-    """
-    assert type(page) == int and type(page_size) == int
-    assert page > 0 and page_size > 0
-    start, end = index_range(page, page_size)
-    data = self.dataset()
-    if start > len(data):
-        return []
-    return data[start:end]
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Retrieves a subset of data corresponding to a specific page number and size
+
+        Return: 
+            A list of lists representing the rows of the dataset for the specified page
+        """
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start > len(data):
+            return []
+        return data[start:end]
