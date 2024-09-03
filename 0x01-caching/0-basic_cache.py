b2e16a65a7446cd53f05f941f92d803df15c3dd5
Roslyn10 <roslynclewis1004@gmail.com>
2024-09-03 15:22:10 +0200

    Adding the code for 0- Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""A class tha inherts from BaseCaching and is a caching system"""
+
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    A Class that Inherits from BAseCahching and is a caching system
+    """
+    def put(self, key, item):
+        """
+        Adds an item to the cache
+
+        Args:
+            key (str): The key under which the item will be stored
+            item (any): The item to be stored in the cache
+        """
+        if key is None or item is None:
+            pass
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Retrieves an item from the cache
+
+        Args:
+            key (str): The key of the item to retrive.
+
+        Returns:
+            any: The item associated with the key,
+            or None if the key does not exist or is None.
+        """
+        if key is not None and key in self.cache_datakeys():
+            return self.cache_data[key]
+        else:
+            return None
