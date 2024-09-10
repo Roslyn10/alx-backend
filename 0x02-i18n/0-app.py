89924667c69ed2db164fdf483d954a2c6f8f8827
Roslyn10 <roslynclewis1004@gmail.com>
2024-09-10 13:29:05 +0200

    Fixing the pycodestyle and declaring render_template to the flask code

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,15 +2,16 @@
 """A Simple flask setup"""
 
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
 
+@app.route('/')
 def index():
     """The index page"""
-    return render_template(0-index.html)
+    return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
