66d81e13145f7c66765eb20ae6c94a2ca131cd07
Roslyn10 <roslynclewis1004@gmail.com>
2024-09-10 13:52:16 +0200

    Fixing the index html file

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,12 +6,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def index():
+def home():
     """The index page"""
     return render_template('0-index.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
