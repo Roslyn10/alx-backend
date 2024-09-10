42bb2e517c75c072fa7aefebf7b032324fc3937f
Roslyn10 <roslynclewis1004@gmail.com>
2024-09-10 21:40:35 +0200

    Adding the code for4, which is to allow the langauge/ website to be able to switch between the english translation and the french

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""A simple flask setup using babel"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+class Config:
+    """Configures available languages in the app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Determines the best language match with supported languages"""
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """The index page"""
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
